65292af8e1d0284fe77f47a4db4cfcea9d9f6504
unknown <john@same.com>
2025-11-08 12:38:51 +0200

    added laukums too

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ const PI = 3.14;
 calcButton.addEventListener('click', handleClick);
 
 function handleClick(){
-    outputContainer.innerText = 2 * PI * radiusInput.value;
+    let radius = radiusInput.value
+    let area = PI * radius * radius;
+    let circumference = 2 * PI * radius;
+    outputContainer.innerText = `Laukums: ${area}; R.L garums: ${circumference}`;
 
     // console.log(radiusInput.value)
 }
